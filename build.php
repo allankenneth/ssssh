3f503fd099ed8304096754a118526f0dc0ec005b
Allan Kenneth <allankh@icloud.com>
2022-07-03 11:59:39 -0700

    try again

--- build.php
+++ build.php
@@ -1,20 +1,17 @@
 <?php 
 $export = 'index.html';
-$bylaws = file_get_contents('https://raw.githubusercontent.com/allankenneth/ssssh/main/noise.json');
-$zones = json_decode($bylaws);
+$data = file_get_contents('https://raw.githubusercontent.com/allankenneth/ssssh/main/noise.json');
+$bylaws = json_decode($data);
 $output = '<h1>' . date('Y-m-d') . '</h1>';
-foreach($zones as $zone) {
-        foreach($zone as $z) {
-                print_r($z);
-                $output .= '<h2>' . $z->name . '</h2>'; 
-                foreach($z->types as $type) {
+foreach($bylaws as $zones) {
+        foreach($zones as $zone) {
+                $output .= '<h2>' . $zone->name . '</h2>'; 
+                foreach($zone->types as $type) {
                         $output .= $type->type . '<br>';
                         $output .= $type->description . '<br>';
                         foreach($type->ranges as $range) {
                                 $output .= $range->name . '<br>';
-                                //print_r($day->Weekdays);
                         }
-
                 }
         }
 }
