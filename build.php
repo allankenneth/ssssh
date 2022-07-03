a38277a9bf3a5b5d713216a4aa82fe64e64f11cc
Allan Kenneth <allankh@icloud.com>
2022-07-03 00:38:13 -0700

    A start

--- build.php
+++ build.php
@@ -1,6 +1,21 @@
 <?php 
 $export = 'noise.html';
-//$noises = json_decode('noise.json');
-$noises = '<h1>Can I make noise outside right now?</h1>';
-file_put_contents($export, $noises);
-echo 'success';
+$bylaws = file_get_contents('https://raw.githubusercontent.com/allankenneth/ssssh/main/noise.json');
+$zones = json_decode($bylaws);
+$output = '<h1>' . date('Y-m-d') . '</h1>';
+foreach($zones as $zone) {
+        foreach($zone as $z) {
+                print_r($z);
+                $output .= '<h2>' . $z->name . '</h2>'; 
+                foreach($z->types as $type) {
+                        $output .= $type->type . '<br>';
+                        $output .= $type->description . '<br>';
+                        foreach($type->days as $day) {
+                                //print_r($day->Weekdays);
+                        }
+
+                }
+        }
+}
+file_put_contents($export, $output);
+echo $output
