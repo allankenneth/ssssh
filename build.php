005be8fc5e2837d8e7c159cce55ad6a91cc93c13
Allan Kenneth <allankh@icloud.com>
2022-07-03 13:11:06 -0700

    Change HTML structure again

--- build.php
+++ build.php
@@ -2,12 +2,13 @@
 $export = 'index.html';
 $data = file_get_contents('https://raw.githubusercontent.com/allankenneth/ssssh/main/noise.json');
 $bylaws = json_decode($data);
-$output = '<h1>' . date('Y-m-d') . '</h1>';
+//$output = '<h1>' . date('Y-m-d') . '</h1>';
+$output = '';
 foreach($bylaws as $zones) {
         foreach($zones as $zone) {
-                $output .= '<h2>' . $zone->name . '</h2>'; 
+                $output .= '<h1>' . $zone->name . '</h1>'; 
                 foreach($zone->types as $type) {
-                        $output .= '<h3>' . $type->type . '</h3>';
+                        $output .= '<h1>' . $type->type . '</h1>';
                         $output .= $type->description . '<br>';
                         foreach($type->ranges as $range) {
                                 $output .= '<h4>' . $range->name . '</h4>';
