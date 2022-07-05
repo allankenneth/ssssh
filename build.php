329ddddbd30e6f44d281cd3815b2403b63154df9
Allan Kenneth <allankh@icloud.com>
2022-07-04 22:08:05 -0700

    Cache control; mobile meta; js refresh on the hour

--- build.php
+++ build.php
@@ -70,8 +70,23 @@ $header = <<<EOD
 <!doctype HTML>
 <html lang="en">
 <head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Expires" content="0">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+<script>
+    var current = new Date();
+    var future = new Date();
+    future.setTime(future.getTime() + 3600000); //3600000 = 1 hour
+    future.setMinutes(0);
+    future.setSeconds(0);
+
+    var timeout = (future.getTime() - current.getTime());
+    setTimeout(function() { window.location.reload(true); }, timeout);
+</script>
 </head>
 <body>
 <div class="container">
